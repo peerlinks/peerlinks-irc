2390c5ad07bfc716ab5a400d03d5645eab2a7ebc
Fedor Indutny <fedor@indutny.com>
2019-08-16 15:01:10 -0700

    src: save progress

--- bin/irc.js
+++ bin/irc.js
@@ -3,11 +3,17 @@ import Server from '../';
 
 const server = new Server();
 
-server.load().then(() => {
+server.open().then(() => {
   server.listen(1337, () => {
     console.error('Listening on %j', server.address());
+  });
+
+  process.on('SIGINT', () => {
+    console.log('Closing...');
+    server.close();
   });
 }).catch((e) => {
   console.error(e.stack);
   process.exit(1);
 });
+
